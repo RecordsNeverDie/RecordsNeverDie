2198734d8243207b6646bafdd73e8a00a755b506
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-04-27 13:44:17 +0200

    change in places route, add view for place details and index and some styles

--- routes/places.routes.js
+++ routes/places.routes.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const router = express.Router()
+const Place = require('../models/place.model')
+const User = require('../models/user.model')
 
-router.get('/places', (req,res) => res.render('places/places-index'))
+// list
+router.get('/', (req,res) => {
+    Place.find()
+    //.populate('user_id')
+    .then(allPlaces => res.render('places/places-index', {places: allPlaces}))
+    .catch(err => console.log(`Ha ocurrido un error: ${err}`)) 
+})
+
+
+// details
+router.get('/details/:id', (req, res, next) => {
+    const placeId = req.params.id 
+
+    Place.findById(placeId)
+    .then(onePlace => res.render('places/places-details', onePlace))
+    .catch(err => console.log(`Ha ocurrido un error: ${err}`)) 
+})
 
 module.exports = router
