170f8d2c574ee42891f9188baf93d371b751c938
Ruben Narvaez <rubnarvaez@gmail.com>
2020-04-28 17:14:20 +0200

    Messing around markers

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,54 +1,66 @@
-initMap = () => {
+let myMap
+window.onload = () => {
 
-    let mapOptions = {
-        center: {
-            lat: 40.416915,
-            lng: -3.702870
-        },
+    const center = {
+        lat: 40.419244,
+        lng: -3.706610
+    } 
+
+//  initMap = () => {
+
+//     let mapOptions = {
+//         center: {
+//             lat: 40.416915,
+//             lng: -3.702870
+//         },
+//         zoom: 15,
+//         styles: mapStyles.retro
+//     }
+
+    myMap = new google.maps.Map(document.getElementById('myMap'), {
         zoom: 15,
+        center,
         styles: mapStyles.retro
-    }
-
-    const myMap = new google.maps.Map(document.getElementById('myMap'), mapOptions)
+    })
 
 
-    let markerOptions = {
-        position: {
-            lat: 40.416930,
-            lng: -3.702807
-        },
-        map: myMap,
-        title: "PRUEBA"
-    }
+    // let markerOptions = {
+    //     position: {
+    //         lat: 40.416930,
+    //         lng: -3.702807
+    //     },
+    //     map: myMap,
+    //     title: "PRUEBA"
+    // }
 
-    new google.maps.Marker(markerOptions)
+    // new google.maps.Marker(markerOptions)
 
-    const markers = []
-    let center = {
-        lat: undefined,
-        lng: undefined,
-    }
+//     const markers = []
+//     let center = {
+//         lat: undefined,
+//         lng: undefined,
+//     }
 
-    // getPin()
+     getPin()
 }
 
 
-// function getPin() {
-//     axios
-//         .get('/places-index')
-//         .then(placesFromApi => {
-//             const places = placesFromApi.data
-//             places.forEach(elm => {
-//                 let center = {
-//                     lat: elm.location.coordinates[0],
-//                     lng: elm.location.coordinates[1]
-//                 }
-//                 new google.maps.Marker({
-//                     position: center,
-//                     map: myMap,
-//                     title: elm.name
-//                 })
-//             })
-//         })
-//         .catch(error => console.log(error))
-// }
+function getPin() {
+    axios
+        .get('/places/api')
+        .then(placesFromApi => {
+            const places = placesFromApi.data
+            places.forEach(elm => {
+                let center = {
+                    lat: elm.location.coordinates[0],
+                    lng: elm.location.coordinates[1]
+                }
+                new google.maps.Marker({
+                    position: center,
+                    map: myMap,
+                    title: elm.name
+                })
+            })
+        })
+        .catch(error => console.log(error))
+}
