a7821302f9b3f99a1b00785ae3a17f2cef74f581
Ruben Narvaez <rubnarvaez@gmail.com>
2020-05-01 09:34:28 +0200

    success view & goodby music

--- app.js
+++ app.js
@@ -16,13 +16,11 @@ require('./configs/middleware.config')(app)
 require('./configs/passport.config')(app)
 require('./configs/views.configs')(app)
 require('./configs/locals.config')(app)
-// require('./configs/spotify.config')(app)
 
 
 // Base URLS
 app.use('/', require('./routes/index.routes'))
 app.use('/', require('./routes/auth.routes'))
-app.use('/music', require('./routes/music.routes'))
 app.use('/places', require('./routes/places.routes'))
 app.use('/shop', require('./routes/shop.routes'))
 
